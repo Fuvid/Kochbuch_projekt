6858504c0f298e629f5a526d2d50499e81b38cb6
David <info@david-amore.de>
2013-11-25 12:19:59 +0100

    Erste Klassen

---
+++ DasUltimativeKochbuch/DasUltimativeKochbuch/Core/Rezept.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DasUltimativeKochbuch.Core
+{
+    class Rezept
+    {
+        List<Zutat> zutaten;
+        String zubereitung;
+        String name;
+        int pers;
+
+
+    }
+}
