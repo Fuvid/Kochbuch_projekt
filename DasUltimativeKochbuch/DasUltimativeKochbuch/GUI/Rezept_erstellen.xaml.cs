54b92cd67d7da6769e064879eb651f9b282bfd46
Tilodon <dorian.fleissig@live.de>
2013-11-26 09:06:32 +0100

    GUI - Neu

---
+++ DasUltimativeKochbuch/DasUltimativeKochbuch/GUI/Rezept_erstellen.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DasUltimativeKochbuch.GUI
+{
+    /// <summary>
+    /// Interaktionslogik für Rezept_erstellen.xaml
+    /// </summary>
+    public partial class Rezept_erstellen : Page
+    {
+        public Rezept_erstellen()
+        {
+            InitializeComponent();
+        }
+    }
+}
