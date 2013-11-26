c3035636beabbd3fb97ac90c9d901c4d2637b5c4
David <info@david-amore.de>
2013-11-26 08:04:03 +0100

    DB-Connector Interface hinzugefügt

---
+++ DasUltimativeKochbuch/DasUltimativeKochbuch/Datenbank/DatenbankConnector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DasUltimativeKochbuch.Core;
+
+namespace DasUltimativeKochbuch.Datenbank
+{
+    interface DatenbankConnector
+    {
+        public void rezSpeichern(Rezept r);
+        public List<Rezept> alleRezepte();
+        public List<Rezept> rezepteMit(List<Zutat> lz);
+
+        public List<Zutat> alleZutaten();
+
+        public void einheitSpeichern(Einheit e);
+        public List<Einheit> alleEinheiten();
+
+    }
+}
