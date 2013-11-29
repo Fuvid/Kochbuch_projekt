0d25021e31f61726a60a1e0088f2c1ed27391760
David <info@david-amore.de>
2013-11-29 10:41:48 +0100

    Anfänge der Suche

--- DasUltimativeKochbuch/DasUltimativeKochbuch/Core/Einheit.cs
+++ DasUltimativeKochbuch/DasUltimativeKochbuch/Core/Einheit.cs
@@ -9,6 +9,10 @@ namespace DasUltimativeKochbuch.Core
     public class Einheit
     {
         public string name{get;set;}
+        public override string ToString()
+        {
+            return name;
+        }
         public Einheit(string n) {
             this.name = n;
         }
