c6d233f25974902c4802010ba92cfc9d8a3311df
chapaou <t-brox@hotmail.de>
2014-05-06 14:53:23 +0200

    dbunittest

---
+++ DasUltimativeKochbuch/DBUnitTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DasUltimativeKochbuch.Core;
+using DasUltimativeKochbuch.Datenbank;
+using System.Collections.Generic;
+
+namespace DBUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void einheitenTest()
+        {
+            //Hier abändern je nach Einträgen in der Datenbank, auf Reihenfolge achten
+            Einheit[] earray = new Einheit[2];
+            earray[0] = new Einheit("Stk");
+            earray[1] = new Einheit("kg");
+
+            DBConnect db = new DBConnect();
+            List<Einheit> el = new List<Einheit>();
+            el = db.alleEinheiten();
+            int i = 0;
+            foreach (Einheit e in el)
+            {
+                string eName = e.name;
+                Assert.AreEqual(eName, earray[i].name);
+                i++;
+            }
+        }
+    }
+}
